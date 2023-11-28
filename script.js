87e170ba398c7708231ce3293cd5f74fd5cc8d29
swimmingcircle119 <lampmaa22@gmail.com>
2023-11-28 11:43:51 -0800

    spanish recognition

--- script.js
+++ script.js
@@ -2,19 +2,54 @@ var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 var SpeechGrammarList = SpeechGrammarList || window.webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
-var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
+const colorTranslations = {
+    'negro': 'black',
+    'blanco': 'white',
+    'rojo': 'red',
+    'verde': 'green',
+    'azul': 'blue',
+    'amarillo': 'yellow',
+    'rosa': 'pink',
+    'naranja': 'orange',
+    'púrpura': 'purple',
+    'gris': 'gray',
+    'marrón': 'brown',
+    'azul marino': 'navy',
+    'verde azulado': 'teal',
+    'plata': 'silver',
+    'lima': 'lime',
+    'aguamarina': 'aqua',
+    'granate': 'maroon',
+    'oliva': 'olive',
+    'coral': 'coral',
+    'fucsia': 'fuchsia',
+    'trigo': 'wheat',
+    'lavanda': 'lavender',
+    'turquesa': 'turquoise',
+    'bronceado': 'tan',
+    'azul cielo': 'skyblue',
+    'salmón': 'salmon',
+    'ciruela': 'plum',
+    'orquídea': 'orchid',
+    'marfil': 'ivory',
+    'oro': 'gold'
+};
+  
+  var spanishColors = Object.keys(colorTranslations);
+  var englishColors = Object.values(colorTranslations);
+  
 
 var recognition = new SpeechRecognition();
 if (SpeechGrammarList) {
   // SpeechGrammarList is not currently available in Safari, and does not have any effect in any other browser.
   // This code is provided as a demonstration of possible capability. You may choose not to use it.
   var speechRecognitionList = new SpeechGrammarList();
-  var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
+  var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + spanishColors.join(' | ') + ' ;'
   speechRecognitionList.addFromString(grammar, 1);
   recognition.grammars = speechRecognitionList;
 }
 recognition.continuous = false;
-recognition.lang = 'en-US';
+recognition.lang = 'es-ES';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
@@ -23,15 +58,21 @@ var bg = document.querySelector('html');
 var hints = document.querySelector('.hints');
 
 var colorHTML= '';
-colors.forEach(function(v, i, a){
-  console.log(v, i);
-  colorHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>';
+Object.entries(colorTranslations).forEach(function(entry, index){
+    let [spanishColor, englishColor] = entry;
+    // console.log(`Spanish: ${spanishColor}, English: ${englishColor}, Index: ${index}`);
+    colorHTML += '<span style="background-color:' + englishColor + ';"> ' + spanishColor + ' </span>';
 });
 hints.innerHTML = 'Tap/click then say a color to change the background color of the app. Try ' + colorHTML + '.';
-
+var isRecognitionRunning = false;
 document.body.onclick = function() {
-  recognition.start();
-  console.log('Ready to receive a color command.');
+if (isRecognitionRunning == false) {
+    recognition.start();
+    isRecognitionRunning = true;
+    console.log('Ready to receive a color command.');
+    }
+//   recognition.start();
+//   console.log('Ready to receive a color command.');
 }
 
 recognition.onresult = function(event) {
@@ -43,9 +84,11 @@ recognition.onresult = function(event) {
   // These also have getters so they can be accessed like arrays.
   // The second [0] returns the SpeechRecognitionAlternative at position 0.
   // We then return the transcript property of the SpeechRecognitionAlternative object
-  var color = event.results[0][0].transcript;
-  diagnostic.textContent = 'Result received: ' + color + '.';
-  bg.style.backgroundColor = color;
+  var spanishColor = event.results[0][0].transcript;
+  var englishColor = colorTranslations[spanishColor];
+  diagnostic.textContent = 'Result received: ' + spanishColor + '.';
+  bg.style.backgroundColor = englishColor;
+  console.log('heard spanishColor: ' + spanishColor, 'change to englishColor: ' + englishColor);
   console.log('Confidence: ' + event.results[0][0].confidence);
 }
 
@@ -56,6 +99,12 @@ recognition.onspeechend = function() {
 recognition.onnomatch = function(event) {
   diagnostic.textContent = "I didn't recognise that color.";
 }
+
+recognition.onend = function() {
+    isRecognitionRunning = false;
+    // Uncomment the next line if you want to automatically restart recognition
+    // recognition.start();
+};
 
 recognition.onerror = function(event) {
   diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
